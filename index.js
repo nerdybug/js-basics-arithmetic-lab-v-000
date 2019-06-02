489cb71f9be8897dd5736a185b95e5cbfb1dbbca
Meredith Reece <mardebugger@gmail.com>
2019-06-02 06:39:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ You should NOT try to redefine any of the above variables.
 */
 
 // Write your code below this line!
+const newID = ""
+const ageIsValid = ""
+const randomNumber = ""
+const randomUserID = ""
